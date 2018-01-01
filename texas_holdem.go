bd779fe891cd1c99cc4b09c0071cfd3cd09bfac8
Eli Gundry <eligundry@gmail.com>
2017-12-31 22:35:55 -0500

    Savepoint

--- texas_holdem.go
+++ texas_holdem.go
@@ -28,6 +28,18 @@ func NewTexasHoldemGame(players *Players) TexasHoldem {
 	return game
 }
 
+func (game *TexasHoldem) Play() {
+	// @TODO Apply the blinds
+	// @TODO Loop through players for initial bets/checks/folds
+	// @TODO Apply the flop
+	// @TODO Loop through players again for second round of bets
+	// @TODO Apply the turn
+	// @TODO Loop through players again for third round of bets
+	// @TODO Apply the river
+	// @TODO Loop through players again for final round of bets
+	// @TODO Determine winner and distribute pot
+}
+
 func (game *TexasHoldem) DealToCommunity(count int) {
 	game.deck.Deal(count, game.community)
 }
